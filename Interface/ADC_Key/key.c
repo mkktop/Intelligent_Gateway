27884e37f62e60124c96e724578444f9970498f8
mkktop <2864078813@qq.com>
2024-12-23 17:27:26 +0800

    修复了按键的已知问题

--- Interface/ADC_Key/key.c
+++ Interface/ADC_Key/key.c
@@ -15,29 +15,34 @@ uint8_t key_GetKey(void)
         if (v < 0.1) {
             return 1;
         }
+        return 0;
     } else if (v < 1.7 && v > 1.6) {
         vTaskDelay(15);
         if (v < 1.7 && v > 1.6) {
             return 2;
         }
+        return 0;
     } else if (v < 2.2 && v > 2.1) {
         vTaskDelay(15);
         if (v < 2.2 && v > 2.1)
         {
             return 3;
-        }                
+        } 
+        return 0;               
     } else if (v < 2.5 && v > 2.4) {
         vTaskDelay(15);
         if (v < 2.5 && v > 2.4)
         {
            return 4;
-        }       
+        }  
+        return 0;     
     } else if (v < 2.7 && v > 2.59) {
         vTaskDelay(15);
         if (v < 2.7 && v > 2.59)
         {
            return 5;
-        }            
+        } 
+        return 0;           
     } else {
         return 0;
     }
